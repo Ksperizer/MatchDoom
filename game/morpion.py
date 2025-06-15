713f0df40b6d11b4be0b05b7b83157819680ec9a
carkevin <kevin.carles@ynov.com>
2025-06-15 16:00:05 +0200

    laision websocket P1 et p2 lier wor wip lancement jeu !

--- game/morpion.py
+++ game/morpion.py
@@ -1,10 +1,43 @@
-import socket 
-import json 
+import json
+import threading
+import websocket
 
 class SocketClient: 
-    def __init__(self, host='localhost', port=9000):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((host, port))
+    def __init__(self, url="ws://localhost:8081/game/ws", pseudo="PlayerX"):
+        self.url = url
+        self.pseudo = pseudo
+        self.ws = websocket.WebSocketApp(
+            self.url, 
+            on_open=self.on_open,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close
+        )
+        self.thread = threading.Thread(target=self.ws.run_forever)
+        self.thread.deamon = True
+        self.thread.start()
+        # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.sock.connect((host, port))
+
+    def on_open(self, ws):
+        print("Connexion WebSocket ouverte")
+        # Send a message 'join'
+        ws.send(json.dumps({
+            "type": "join",
+            "pseudo": self.pseudo
+        }))
+        
+    def on_message(self, ws, message):
+        print("Message reçu:", message)
+        # Handle incoming messages here
+        data = json.loads(message)
+        if data["type"]== "game_start":
+           print(f"Partie contre {data['opponent']} a commencé !")
+        elif data["type"] == "move_played":
+            print(f"{data['player']} a joué à la position {data['row']}, {data['col']}")
+
+
+                 
 
     def send_move(self, row, col):
         message = json.dumps({
@@ -23,5 +56,3 @@ class SocketClient:
         
     def close(self):
         self.sock.close()
-
-    
--- game/websocket.py
+++ game/websocket.py
@@ -6,6 +6,8 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 clients = set()
+players_queue = []
+game = {}
 
 async def handle_player(websocket):
     logging.info("Nouvelle connexion webSocket {websocket.path}")
@@ -25,27 +27,72 @@ async def handle_player(websocket):
         logging.info(f"Connexion fermée: {e}")
     finally:
         clients.remove(websocket)
+
+def remove_from_queue(ws):
+    global players_queue
+    players_queue = [p for p in players_queue if p["ws"] != ws]
     
 async def handle_message(data, websocket):
     msg_type = data.get("type")
 
     if msg_type == "ping":
-        return {"type": "pong"}
+        await websocket.send(json.dumps({"type": "pong"}))
+
     elif msg_type == "join":
         pseudo = data.get("pseudo")
         logging.info(f"Pseudo {pseudo} a rejoint la file d'attente")
-        return {
+
+        # check if pseudo is provided
+        if any(p["pseudo"] == pseudo for p in players_queue):
+            await websocket.send(json.dumps({"type": "error", "message": "Pseudo déjà utilisé"}))
+            return
+
+        players_queue.append({"pseudo": pseudo, "ws": websocket})
+        await websocket.send(json.dumps({
             "type": "queue",
             "message": "En attente d'un adversaire",
-            "position": len(clients)
-        }
-    elif msg_type == "test":
-        return {"type": "info", "message":  "Serveur python en ligne "}
+            "position": len(players_queue)
+        }))
+
+        if len(players_queue) >= 2:
+            await start_game()
+
     else:
-        return {"type": "error", "message": "message non reconnu"}
+        await websocket.send(json.dumps({"type": "error", "message": "Message non reconnu"}))
+
+async def start_game():
+    p1 = players_queue.pop(0)
+    p2 = players_queue.pop(0)
+
+    logging.info(f"Partie commencée entre {p1['pseudo']} et {p2['pseudo']}")
+
+    game_id = f"{p1['pseudo']}_vs_{p2['pseudo']}"
+    board = [""]* 9  # Initialize a 3x3 board
+
+    game[game_id] = {
+        "player1" : p1,
+        "player2" : p2,
+        "board": board,
+        "turn" : p1["pseudo"],  # Player 1 starts
+    }
+
+    await p1["ws"].send(json.dumps({
+        "type": "game_start",
+        "game_id": game_id,
+        "opponent": p2["pseudo"],
+        "symbol": "X",
+        "your_turn": True
+    }))
+    await p2["ws"].send(json.dumps({
+        "type": "game_start",
+        "game_id": game_id,
+        "opponent": p1["pseudo"],
+        "symbol": "O",
+        "your_turn": False
+    }))
 
 async def main():
-    logging.info("Debut du serveur WebSocket sur ws://localhost:8081")
+    logging.info("Début du serveur WebSocket sur ws://localhost:8081")
     async with websockets.serve(handle_player, "0.0.0.0", 8081):
         await asyncio.Future() # run forever
 
